a56df6f2b08ba3bda78f37460f944313873617f8
waikei1917 <halcaim@172-13-8-148.lightspeed.wchtks.sbcglobal.net>
2016-08-09 17:13:18 +0800

    Variables

---
+++ HelloPython/code/Basic/Variables.py
@@ -0,0 +1,12 @@
+print("variables test~~");
+
+firstNumber = 555;
+secondNumber = 888;
+
+print(firstNumber+secondNumber);
+
+thirdNumber = firstNumber+secondNumber;
+
+print(thirdNumber);
+print('thirdNumber id: ', id(thirdNumber));
+print('thirdNumber type: ', type(thirdNumber));
