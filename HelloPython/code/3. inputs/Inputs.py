74281abf01741710fb606b07f302418edae4ea82
waikei1917 <halcaim@172-13-8-148.lightspeed.wchtks.sbcglobal.net>
2016-08-10 21:48:54 +0800

    inputs

---
+++ HelloPython/code/3. inputs/Inputs.py
@@ -0,0 +1,10 @@
+print('Hello Alex')
+input('Press Enter key to gp on')
+print('Thank you!')
+
+data = input('what\'s your name? \n')
+print('Hello, {}'.format(data))
+
+data = input('What is the number? \n')
+data = int(data)*20
+print('The answer is {}'.format(data))
