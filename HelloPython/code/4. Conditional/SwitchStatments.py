8e469e078fc0af861457dafc0626adde9d67ea33
waikei1917 <halcaim@172-13-8-148.lightspeed.wchtks.sbcglobal.net>
2016-08-11 12:44:07 +0800

    Switch

---
+++ HelloPython/code/4. Conditional/SwitchStatments.py
@@ -0,0 +1,12 @@
+switch = dict(
+              one = 'one1',
+              two = 'two2',
+              three = 'three3'
+              )
+
+var = 'one'
+
+print(switch[var])
+print(switch.get(var,'default'))
+
+print(switch['three'])
