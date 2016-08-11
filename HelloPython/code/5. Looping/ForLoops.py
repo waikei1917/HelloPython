90063d10e4f373f58dd3fe8a2dbafe89210385fb
waikei1917 <halcaim@172-13-8-148.lightspeed.wchtks.sbcglobal.net>
2016-08-11 13:01:23 +0800

    ForLoops

---
+++ HelloPython/code/5. Looping/ForLoops.py
@@ -0,0 +1,12 @@
+for data in [1,2,3,4,5]:
+    print(data)
+    
+datadata = [1,2,3,4,5]
+print(datadata)
+
+for data in 'string':
+    print(data)
+    
+for key,data in enumerate('string'):
+    if key %2 == 0:
+        print(data)
