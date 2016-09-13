ea683c01ab6062719d178d246a8deb71e93bd29b
waikei1917 <halcaim@localhost>
2016-09-13 23:53:55 +0800

    TextFile Handling

--- HelloPython/code/7. FileHandling/TextFileHandling.py
+++ HelloPython/code/7. FileHandling/TextFileHandling.py
@@ -8,4 +8,16 @@ output = open('newtxt.txt','w')
 for line in input:
     print(line, file=output, end='')
     
+#write to anther file
+buffersize = 10
+infile = open('text.txt','r')
+outfile = open('newBigFile.txt','w')
+
+buffer = infile.read(buffersize)
+bufferLimit = 500000
+
+while bufferLimit >0:
+    outfile.write(buffer)
+    #print('.', end='')
+    bufferLimit = bufferLimit - buffersize
     
