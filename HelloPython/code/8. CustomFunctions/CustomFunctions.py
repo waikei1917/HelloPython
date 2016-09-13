902cb64529456b0aaf8160951958bc3e05fc18fd
waikei1917 <halcaim@192.168.1.72>
2016-09-13 17:47:10 -0600

    Custom function

---
+++ HelloPython/code/8. CustomFunctions/CustomFunctions.py
@@ -0,0 +1,14 @@
+def function_name():
+    print('this is a timeline')
+    return "hello"
+
+print(function_name())
+
+list1 = [1,2,3,4]
+
+def function_name2(lists):
+    lists.append([5,6,7,8])
+    return
+
+function_name2(list1)
+print(list1)
---
+++ HelloPython/code/8. CustomFunctions/CustomFunctionsReturn.py
@@ -0,0 +1,13 @@
+def function_return():
+    return 5
+
+print(function_return())
+
+def function_returnBool():
+    return;
+
+var = function_returnBool()
+if var==None:
+    print('true')
+    
+    
